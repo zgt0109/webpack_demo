38e657d77e88256303bf8b67cbd1568a4460b6ae
zouguotao <2506526350@qq.com>
2018-07-02 16:34:09 +0800

    webpack guide 管理资源+1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
+import printMe from './print.js';
 
 console.log('Hello World!')
 console.log('Hello World111!')
@@ -10,16 +11,22 @@ console.log('Hello World222!')
 
 function component(){
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello','Webpack','!']);
-    element.classList.add('hello');
+    // element.innerHTML = _.join(['Hello','Webpack','!']);
+    // element.classList.add('hello');
 
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    btn.innerHTML('Click me and check the console ');
+    btn.onclick = printMe;
 
-    console.log(Data);
+    element.appendChild(btn);
+
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
+    // element.appendChild(myIcon);
+
+    // console.log(Data);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
   devServer:{
     contentBase: './dist',
     host: '0.0.0.0',
     port: '4000'
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
         title: 'Output Management'
     })
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module:{
